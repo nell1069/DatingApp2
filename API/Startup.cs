a1ebf52a6df06c43b1e9243ba4a35181c2d6f0a1
Will <williamg_moore@yahoo.com>
2022-04-18 08:33:00 -0500

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,16 +33,15 @@ private readonly IConfiguration _config;
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-             services.AddControllers();
+            
              services.AddDbContext<DataContext>(optins =>
             {
                 optins.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });  
            
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            services.AddControllers();
+            services.AddCors();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,13 +50,13 @@ private readonly IConfiguration _config;
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
